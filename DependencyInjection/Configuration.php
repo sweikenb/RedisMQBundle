712aa481efe8c7912978baefd4d25b37f58066fe
Simon Schröer <ssc@basecom.de>
2014-03-07 08:34:29 +0100

    Cleanup

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -18,7 +18,7 @@ class Configuration implements ConfigurationInterface
     public function getConfigTreeBuilder()
     {
         $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('sweikenb_redis_mq');
+//        $rootNode = $treeBuilder->root('sweikenb_redis_mq');
 
         // Here you should define the parameters that are allowed to
         // configure your bundle. See the documentation linked above for
--- DependencyInjection/SweikenbRedisMQExtension.php
+++ DependencyInjection/SweikenbRedisMQExtension.php
@@ -19,10 +19,10 @@ class SweikenbRedisMQExtension extends Extension
      */
     public function load(array $configs, ContainerBuilder $container)
     {
-        $configuration = new Configuration();
-        $config = $this->processConfiguration($configuration, $configs);
+//        $configuration = new Configuration();
+//        $config = $this->processConfiguration($configuration, $configs);
 
-        $loader = new Loader\XmlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+        $loader = new Loader\XmlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
         $loader->load('services.xml');
     }
 }
